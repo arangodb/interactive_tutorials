e934a0b97dc77faff37a5db11990fbfa00562f09
Chris Woodward <cw00dw0rd@gmail.com>
2022-03-17 19:28:01 -0400

    Update import_hybrid.sh

--- import_hybrid.sh
+++ import_hybrid.sh
@@ -45,7 +45,7 @@ run_import () {
   local file="$2"
   local type="$3"
   shift 3
-  $ARANGOIMPORT --type csv --collection "$collection" --separator "|" --create-collection true --create-collection-type "$type" --file "$file" --server.endpoint "$ARANGO_ENDPOINT" --server.database "$ARANGO_DATABASE" --server.username "$ARANGO_USERNAME" --server.password "$ARANGO_PASSWORD" "$@" --overwrite true
+  $ARANGOIMPORT --type csv --collection "$collection" --separator "|" --create-collection true --create-collection-type "$type" --file "$file" --server.endpoint "$ARANGO_ENDPOINT" --server.database "$ARANGO_DATABASE" --server.username "$ARANGO_USERNAME" --server.password "$ARANGO_PASSWORD" "$@"
   import_result=$?
 }
 
